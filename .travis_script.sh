9e2041dd6fafc3b27254c5e1c704343d912331d3
Chris MacMackin <chris.macmackin@ukaea.uk>
2020-02-20 16:51:56 +0000

    Trying CI with default Python3.7 and compilers on Ubuntu

--- .travis_script.sh
+++ .travis_script.sh
@@ -9,7 +9,7 @@ then
     cd ${TRAVIS_BUILD_DIR}
     # Run c++ and python unit tests
     ./build/bin/peakingduckcpptests
-    coverage run --source ./peakingduck -m pytest --tb=long ./tests/py/testsuite.py
+    $PY_CMD -m coverage run --source ./peakingduck -m pytest --tb=long ./tests/py/testsuite.py
 else
     unset PYTHONPATH
     $PY_CMD -m pip install --user .
