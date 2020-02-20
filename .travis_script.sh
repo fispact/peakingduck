dd2886fdd9f22be39d4b8659f50e211f9861c121
Chris MacMackin <chris.macmackin@ukaea.uk>
2020-02-20 09:48:12 +0000

    Fixed CI config to use external script file

---
+++ .travis_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ${BUILD_TYPE} != "Install" ]
+then
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_TESTS=ON -DBUILD_PY_BINDINGS=ON ..
+    make -j4
+    cd ${TRAVIS_BUILD_DIR}
+    # Run c++ and python unit tests
+    ./build/bin/peakingduckcpptests
+    coverage run --source ./peakingduck -m pytest --tb=long ./tests/py/testsuite.py
+else
+    unset PYTHONPATH
+    $PY_CMD -m pip install --user .
+    pytest ./tests/py/testsuite.py
+    $PY_CMD setup.py test
+    # TODO: Test CMake installation
+fi
