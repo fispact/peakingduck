984c94636f40e38a93581b8a0bb8e56e1826c4b1
Chris MacMackin <chris.macmackin@ukaea.uk>
2020-02-24 16:26:47 +0000

    Trying to make CMake find correct Python on Windows CI

--- .travis_script.sh
+++ .travis_script.sh
@@ -4,7 +4,7 @@ if [ ${BUILD_TYPE} != "Install" ]
 then
     mkdir build
     cd build
-    cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_TESTS=ON -DBUILD_PY_BINDINGS=ON .. || exit -1
+    cmake -DPYTHON_EXECUTABLE:FILEPATH=${PY_PATH} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_TESTS=ON -DBUILD_PY_BINDINGS=ON .. || exit -1
     make -j4 || exit -1
     cd ${TRAVIS_BUILD_DIR} || exit -1
     # Run c++ and python unit tests
