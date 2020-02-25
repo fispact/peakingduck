3a264898b8a14df27f0ea05cb0d0ea9c4a394c92
thomasms <stainer.tom@gmail.com>
2020-02-25 13:22:58 +0000

    Added a prototype windowing peak finder

--- peakingduck/core/peaking.py
+++ peakingduck/core/peaking.py
@@ -5,7 +5,7 @@ import math
 import array
 
 # raw C++ bindings library
-from PEAKINGDUCK.core import IPeakFinder, PeakInfo, NumericalData
+from PEAKINGDUCK.core import IPeakFinder, PeakInfo, NumericalData, window
 from .smoothing import SavitzkyGolaySmoother
 
 """
@@ -100,4 +100,71 @@ class ScipyPeakFinder(IPeakFinder):
         for i in indices:
             peaks.append(PeakInfo(i, data[i]))
 
+        return peaks
+
+class WindowPeakFinder(IPeakFinder):
+    """
+        A bespoke window method peak finder
+    """   
+    def __init__(self, threshold=2.0, ninner=0, nouter=40, 
+                    include_point=False, enforce_maximum=False, use_grad=False):
+        # number of sigma
+        self.threshold = threshold
+        self.ninner = ninner
+        self.nouter = nouter
+        self.include_point = include_point
+        self.enforce_maximum = enforce_maximum
+        self.use_grad = use_grad
+
+    def find(self, data, *args, **kwargs):
+        """
+            Takes npoints either side of bin for each bin in histogram 
+            to get mean and stddev with and without that bin
+
+            Doesn't use the gradient yet, but we should enable this
+        """
+        grad = None
+        if self.use_grad:
+            grad = data.gradient(order=1)
+
+        start = 3
+        stop = len(data)-4
+
+        #need at least twice as many bin values as npoints
+        assert len(data) > self.nouter*2
+
+        # find peaks
+        peaks = []
+        for i, value in enumerate(data):
+            if i < start:
+                continue
+            if i > stop and stop > 0:
+                continue
+
+            # compute mean + sigma with point
+            values = window(data, i, nouter=self.nouter, ninner=self.ninner, includeindex=self.include_point)
+            meanvalue, stddev = values.mean(), values.stddev(ddof=1)
+            localthreshold = meanvalue + stddev*self.threshold 
+
+            # first check 2 points either side are also bigger than threshold
+            if (localthreshold < value) \
+                and (localthreshold <= data[i+1]) \
+                and (localthreshold <= data[i-1]):
+                # and (localthreshold <= data[i+2]) \
+                # and (localthreshold <= data[i-2]):
+
+                if self.enforce_maximum and \
+                    ((data[i+1] > value) \
+                    or (data[i-1] > value)):
+                    continue
+
+                # not gradient applied
+                if not grad:
+                    peaks.append(PeakInfo(i, value))
+                # look at first derivative to check for + -> -
+                # we only want positive peaks
+                else:
+                    #TODO: implement using grad
+                    peaks.append(PeakInfo(i, value))
+            
         return peaks
