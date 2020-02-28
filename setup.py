b7998e337fd250de728c524229c8630338772db5
Chris MacMackin <chris.macmackin@ukaea.uk>
2020-02-28 10:51:41 +0000

    Trying to fix pip build on Windows CI

--- setup.py
+++ setup.py
@@ -88,10 +88,9 @@ class CMakeBuild(build_ext):
             cmake_args.append('-DCMAKE_MAKE_PROGRAM=mingw32-make')
             #if sys.maxsize > 2**32:
             #    cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j8']
+            #build_args += ['--', '/m']
+        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+        build_args += ['--', '-j8']
 
         # do not build tests for python install
         cmake_args += ['-DBUILD_TESTS=OFF']
